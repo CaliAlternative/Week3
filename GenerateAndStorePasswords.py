5657ac67caf17e790dadd5595db7cc2f8ff0cb8b
Anthony Jackson <aj213@humboldt.edu>
2022-03-02 13:41:58 -0800

    Complete but needs pytest

--- GenerateAndStorePasswords.py
+++ GenerateAndStorePasswords.py
@@ -1,22 +1,31 @@
-from email.generator import Generator
+#from email.generator import Generator
 import random
 import csv
 class PasswordGenerator:
-    def Generator(self, passwordLength: int):
+    def __init__(self, passwordLength: int):
+        self.passwordLength = passwordLength
+        
+    
+    
+    def Generator(self):
         lower = "abcdefghijklmnopqrstuvwxyz"
         upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
         numbers = "0123456789"
         symbols = "_!&"
         all = lower + upper + numbers + symbols
-        length = passwordLength 
+        length = self.passwordLength 
         password = " ".join(random.sample(all,length))
         print(password)
         return password 
     
-class writeANDstorePassword    
-    def savePassword(self, password_toSave: str):
-        with open('savePassword.txt', 'w') as PasswordText:
-         PasswordText.write(password_toSave)   
+class writeANDstorePassword(PasswordGenerator): 
+    def __init__(self, NewPassword: str):
+        self.NewPassword = NewPassword
     
-NewPassword = Generator(8)
+       
+    def savePassword(self):
+        with open('savePassword.txt', 'w') as PasswordText:
+         PasswordText.write(NewPassword)   
+    
+NewPassword = PasswordGenerator(8)
 SavePassword = writeANDstorePassword(NewPassword)
