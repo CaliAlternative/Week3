bf62c44a61f82ece618624b202fbf2e3bf9a767f
Anthony Jackson <aj213@humboldt.edu>
2022-03-03 02:48:16 -0800

    Updated but not tested

--- CohortProgram.py
+++ CohortProgram.py
@@ -3,44 +3,46 @@ import random
 
 class Dice:
       
-    def __init__(self, dice1: int, dice2: int):
-          self.roll_dice1 = dice1
-          self.roll_dice2 = dice2
+    def __init__(self, firstdice: int, seconddice: int):
+          self.firstdice = firstdice
+          self.seconddice = seconddice
 
-      
-    def rolldice(firstdice, seconddice):  
+     
+    def rolldice(self,firstdice,seconddice):  
           list1 = [1, 2, 3, 4, 5, 6]
           firstdice = random.choice(list1)
           seconddice = random.choice(list1)
           return(firstdice, seconddice)
     
 
-class Player:
-    def __init__(self, _firstdice: int, _seconddice: int):
-         super().__init__(Dice.firstdice, Dice.seconddice)
+class Player(Dice):
+
+    def __init__(self, firstdice, seconddice):
+         super().__init__(firstdice, seconddice)
          
-         _firstdice = Dice.firstdice
-         _seconddice = Dice.seconddice  
+         firstdice = self.firstdice
+         seconddice = self.seconddice  
            
-    def winner(_firstdice, _seconddice):
-        if _firstdice + _seconddice = 7:
+    def winner(self,firstdice, seconddice):
+        firstdice = self.firstdice
+        seconddice = self.seconddice
+        if self.firstdice + self.seconddice == 7:
             print("winner")
-        elif _firstdice + _seconddice = 11:
+        elif firstdice + seconddice == 11:
             print("11 is a winner")
-        elif _firstdice + _seconddice = 2:
+        elif firstdice + seconddice == 2:
             print("loser")
-        elif _firstdice + _seconddice = 12:
+        elif firstdice + seconddice == 12:
             print("12 is a loser")
         else:
             print("Try again")
 
 
-
 dice1 = 0
 dice2 = 0
+Dice()
 Dice.rolldice(dice1, dice2)
 print(dice1,dice2)
 print(Player.winner(dice1,dice2))
-
-
+print(help(Dice))
           
